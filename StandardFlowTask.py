75e097a3717cae3d4f8dea935e8475e2ea6a41ef
masahirotoriumi <m.toriumi@gmail.com>
2017-02-05 16:48:31 +0900

    ログだし。

--- StandardFlowTask.py
+++ StandardFlowTask.py
@@ -1,5 +1,4 @@
-from luigi import Task, WrapperTask, run
-from luigi.format import UTF8
+from luigi import Task, run
 from luigi.mock import MockTarget
 from inspect import currentframe
 
@@ -8,19 +7,41 @@ class DependentTask(Task):
 
     def output(self):
         print("running {}.{}".format(self.__class__.__name__, currentframe().f_code.co_name))
-        return MockTarget('out.txt', format=UTF8)
+        return MockTarget('out.txt')
 
     def run(self):
         print("running {}.{}".format(self.__class__.__name__, currentframe().f_code.co_name))
         with self.output().open('w') as fout:
-            fout.write('DependentTask is Succeeded')
+            fout.write('DependentTask is succeeded')
+
+    def on_success(self):
+        print("Reached {}.{}".format(self.__class__.__name__, currentframe().f_code.co_name))
+
+    def on_failure(self, exception):
+        print("Reached {}.{} {}".format(self.__class__.__name__, currentframe().f_code.co_name, exception))
 
 
-class StartTask(WrapperTask):
+class StartTask(Task):
+
+    def output(self):
+        print("running {}.{}".format(self.__class__.__name__, currentframe().f_code.co_name))
+        return MockTarget("StartTaskOut.txt")
 
     def requires(self):
         print("running {}.{}".format(self.__class__.__name__, currentframe().f_code.co_name))
         return DependentTask()
+
+    def run(self):
+        print("running {}.{}".format(self.__class__.__name__, currentframe().f_code.co_name))
+        with self.output().open('w') as fout:
+            fout.write('StartTask is succeeded')
+
+    def on_success(self):
+        print("Reached {}.{}".format(self.__class__.__name__, currentframe().f_code.co_name))
+
+    def on_failure(self, exception):
+        print("Reached {}.{} {}".format(self.__class__.__name__, currentframe().f_code.co_name, exception))
+
 
 
 if __name__ == '__main__':
